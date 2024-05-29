e25bf6d30a48a44e533f2fda0f2296db4dab388b
TheSh4h <hh.haseebshah@gmail.com>
2024-05-29 23:16:25 +0500

    schema created

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
+// connect to db
+const dbURI = 'mongodb+srv://haseebshah:test1234@cluster0.b1qbvue.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(dbURI, { useNewURLParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
