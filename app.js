4276907d76e445845674520de1c03c82ab395951
TheSh4h <hh.haseebshah@gmail.com>
2024-06-05 12:33:34 +0500

    post request

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
@@ -35,6 +36,18 @@ app.get('/blogs', (req, res) => {
             res.render('index', { title: 'All Blogs', blogs: result })
         })
         .catch((err) =>{
+            console.log(err);
+        })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
             console.log(err);
         })
 })
